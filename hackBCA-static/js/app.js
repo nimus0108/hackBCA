9ddf8326bc6232e5e887a64d6babaef867b48495
Sam Olaogun <hackday@Sams-MacBook-Pro-2.local>
2016-11-24 11:42:09 -0500

    Reorganized sass, fixed mobile buttton and other things

--- hackBCA-static/js/app.js
+++ hackBCA-static/js/app.js
@@ -1,16 +1,44 @@
 $(document).ready(function () {
+
+	var $confirmation = $('#confirmation');
+	var $submit = $('input[type="submit"]');
+	var $email = $('input[type="submit"]');
+
 	align();
+
+	//$confirmation.css("transform", "translateY(" + toPx($confirmation.outerHeight() * -1) + ")");
+
+	// Validate first
+
+	/* $submit.click(function () {
+			var emailValue = $email.value();
+			$confirmation.velocity({
+				translateY: "0px"
+			}, {
+				easing: [20, 10]
+			});
+		}); */
+
 });
 
 function align() {
 
-	$('#line_top').css({
-		'margin-top': nudge($('#line_top').height() / 2, '-')
+	$('#line__top').css({
+		'margin-top': nudge($('#line__top').height() / 2, '-')
 	});
-	$('#line_bottom').css({
-		'margin-bottom': nudge($('#line_bottom').height() / 2, '-')
+	$('#line__bottom').css({
+		'margin-bottom': nudge($('#line__bottom').height() / 2, '-')
 	});
 
+}
+
+function validateEmail(email) {
+	var reg = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+	return reg.test(email);
+}
+
+function toPx(value) {
+	return value + 'px';
 }
 
 function nudge(e, s) {
