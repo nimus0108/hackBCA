f28a1d47ee6460a04e689730c2bf3c2e5f2f5621
Sam Olaogun <sam.olaogun@gmail.com>
2016-12-29 13:22:28 -0500

    Updated script
    cleaned up the script, next: sponsors, faq, and other stuff

--- hackBCA-home/app.js
+++ hackBCA-home/app.js
@@ -2,32 +2,47 @@
 
 $(function () {
 
+	var DATA_PATH = 'data.json';
 
-	var dataPath = 'data.json';
-	
 	$('.faq').click(function () {
 		$(this).toggleClass('flipped');
 	});
 
-	$.getJSON(dataPath, function (data) {
+
+	$.getJSON(DATA_PATH, function (data) {
 
 		var guests = data.guests
 			, sponsors = data.sponsors
-			, faq = data.faq;
-
-		var $guests = $('#guest__container')
+			, faq = data.faq
+			, $guests = $('#guest__container')
 			, $sponsors = $('#sponsors__container')
 			, $faq = $('#faq__container');
 
 		(function () {
 			for (var i = 0, len = guests.length; i < len; i++) {
-				$guests.append('<div class="guest"><figure class="guest__portrait"></figure><hgroup><span class="guest__role">' + guests[i].role + '</span><h1 class="guest__name">' + guests[i].name + '</h1><h2 class="guest__description">' + guests[i].description + '</h2></hgroup></div>');
+				$guests.append('\
+				<div class="guest">\
+					<figure class="guest__portrait"></figure>\
+					<hgroup>\
+						<span class="guest__role">' + guests[i].role + '</span>\
+						<h1 class="guest__name">' + guests[i].name + '</h1>\
+						<h2 class="guest__description">' + guests[i].description + '</h2>\
+					</hgroup>\
+				</div>');
 			}
 		})();
 
 		(function () {
-			for (var i = 0, len = guests.length; i < len; i++) {
-				$faq.append('<div class="faq__container"><div class="faq"><div class="faq__front"><h1 class="faq__question"></h1></div><div class="faq__back"><p class="faq__answer"></p></div></div>');
+			for (var i = 0, len = faq.length; i < len; i++) {
+				$faq.append('\
+				<div class="faq__container">\
+					<div class="faq"><div class="faq__front">\
+						<h1 class="faq__question">' + faq[i].question + '</h1>\
+					</div>\
+					<div class="faq__back">\
+						<p class="faq__answer">' + faq[i].answer + '</p>\
+					</div>\
+				</div>');
 			}
 		})();
 
