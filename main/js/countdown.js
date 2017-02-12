b4fc484818ab9d2ff24ffb7698db0cd7c5cf779a
nimus0108 <nimus0108@gmail.com>
2017-02-11 23:35:03 -0500

    time change

--- main/js/countdown.js
+++ main/js/countdown.js
@@ -35,5 +35,5 @@ function initializeClock(id, endtime) {
   var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline = '2017-05-16T15:00:00.000Z';
+var deadline = '2017-03-25T15:00:00.000Z';
 initializeClock('countdown', deadline);
